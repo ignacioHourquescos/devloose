7178c1299238adfaf363435223ae8de699d21871
Jimancio <54536722+ignacioHourquescos@users.noreply.github.com>
2021-12-29 00:39:14 -0300

    minimal

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,13 @@
 import ss from './index.module.scss'
 import CardContainer from '../components/CardContainer/CardContainer'
 import Profile from '../components/Profile/Profile'
+import Head from 'next/head'
 
 export default function Home() {
   return (
      <>
+      <Head>
+      </Head>
       
     <div className={ss.container}>
 
