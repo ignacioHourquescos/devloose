f74e60973af4edcb81a6d2da4fe75303c83e0195
Jimancio <54536722+ignacioHourquescos@users.noreply.github.com>
2022-04-26 02:35:19 -0300

    scope 002

---
+++ pages/xyz/002/index.js
@@ -0,0 +1,32 @@
+
+import Head from 'next/head'
+
+
+
+const Footer = () => {
+
+
+    return (
+        <>
+              <Head>
+         <title>WEBALUX</title>
+         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+         <meta name="description" content="Definicion Alcance" />
+          <meta property="og:title" content="Definición alcance" />
+         <meta property="og:description" content="Agencia creativa digital" />
+         <meta property="og:image" content="/kaleillustration.jpg" /> 
+      </Head>
+         <div style={{display:"flex", justifyContent:"center", justifyItems:"center"}}>
+      <iframe style={{height:"100vh", width:"100%"}} 
+      src="https://docs.google.com/document/d/e/2PACX-1vQAnZQSxRI9ZbKE9zxSCFq1VAd4K_3U0_J4-_qA5y7Sl_UydqHTiGjbnn4RlcZq5YAWKDyaeGto7t8A/pub?embedded=true"></iframe>
+      </div>
+	
+{/* style={{height:"100vh", width:"100vw"}}></iframe> */}
+        </>
+
+
+    )
+
+}
+
+export default Footer;
