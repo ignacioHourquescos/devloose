f47c1159976bb8f52c1105b423b2ae189bab9685
Jimancio <54536722+ignacioHourquescos@users.noreply.github.com>
2022-12-29 23:10:28 -0300

    cambios logo

--- UI/Logo/Logo.js
+++ UI/Logo/Logo.js
@@ -4,8 +4,8 @@ import { StyledLogo } from "./styles.js";
 const Logo = () => {
 	return (
 		<StyledLogo.Inner>
-			{/* <StyledLogo.Title>DEVLOOSE</StyledLogo.Title>
-			<StyledLogo.Subtitle>DIGITAL FACTORY</StyledLogo.Subtitle> */}
+			<StyledLogo.Title>DEVLOOSE</StyledLogo.Title>
+			<StyledLogo.Subtitle>DIGITAL FACTORY</StyledLogo.Subtitle>
 		</StyledLogo.Inner>
 	);
 };
