fac2c5df4df44310c7a522386b87e44b9cdc7851
Maximiliano Ezequiel Acuña <thompssonlite@gmail.com>
2023-07-02 18:47:57 -0300

    nuevo formulario

--- pages/contact/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { Styled } from "./styles";
-
-const Contact = (props) => {
-	return <Styled.Inner>Contact</Styled.Inner>;
-};
-
-export default Contact;
--- pages/contact/styles.js
+++
@@ -1,6 +0,0 @@
-import styled from "styled-components";
-export const Styled = {
-	Inner: styled.div`
- flex-1
-  `,
-};
