70950590ab9b3385db1273da5f9f5567b6b38cce
unknown <ihourquescos@gmail.com>
2023-08-02 13:12:03 -0300

    armo ruta de contacto

--- pages/contact/styles.js
+++ pages/contact/styles.js
@@ -1,22 +1,4 @@
 import styled from "styled-components";
-
-export const Presentacion = styled.div`
-	height: 100vh;
-	width: 100%;
-	display: flex;
-	justify-content: center;
-`;
-
-export const Container = styled.div`
-	margin-top: 3rem;
-`;
-
-export const Formulario = styled.div`
-	margin-bottom: 2.7rem;
-	margin-left: 2rem;
-`;
-
-export const Boton = styled.div`
-	margin-left: 68%;
-	margin-top: 2rem;
-`;
+export const Styled = {
+	Inner: styled.div``,
+};
